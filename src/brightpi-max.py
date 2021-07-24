399778b3fdf24ecaac42f191e0b94dd5cf7374f5
johncass <john.cass77@gmail.com>
2021-07-24 20:35:57 +0200

    enh: More startup effects.

--- src/brightpi-max.py
+++ src/brightpi-max.py
@@ -9,13 +9,18 @@ brightPi = BrightPi()
 print(f"Setting gain to max ({BrightPi._max_gain})...")
 brightPi.set_gain(BrightPi._max_gain)
 
-print(f"Setting LED's to max brightness ({BrightPi._max_dim})...")
-brightPi.set_led_dim(LED_ALL, BrightPi._max_dim)
+brightPi.set_led_dim(LED_ALL, 0x00)
 
 print("Turning on LED's...")
 brightPi.set_led_on_off(LED_IR, ON)
 for led in LED_WHITE:
     brightPi.set_led_on_off((led,), ON)
     time.sleep(0.5)
+
+print(f"Setting LED's to max brightness ({BrightPi._max_dim})...")
+dim = 0x00
+while dim < BrightPi._max_dim:
+    dim += 1
+    brightPi.set_led_dim(LED_ALL, dim)
 
 print("BrightPi is at max!")
