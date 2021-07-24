40aa4a7cdfeab58a09c4a2010e1ee1937f9484d6
johncass <john.cass77@gmail.com>
2021-07-24 20:22:48 +0200

    enh(led): Add turn on / off effects.

--- src/brightpi-max.py
+++ src/brightpi-max.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 # Turn on all LEDs and set brightness to max.
+import time
 
-from brightpi.brightpilib import BrightPi, LED_ALL, ON
+from brightpi.brightpilib import BrightPi, LED_ALL, LED_IR, LED_WHITE, ON
 
 brightPi = BrightPi()
 
 print(f"Setting gain to max ({BrightPi._max_gain})...")
 brightPi.set_gain(BrightPi._max_gain)
 
-print("Turning LEDs on...")
-brightPi.set_led_on_off(LED_ALL, ON)
-
 print(f"Setting LED's to max brightness ({BrightPi._max_dim})...")
 brightPi.set_led_dim(LED_ALL, BrightPi._max_dim)
+
+print("Turning on LED's...")
+brightPi.set_led_on_off(LED_IR, ON)
+for led in LED_WHITE:
+    brightPi.set_led_on_off(led, ON)
+    time.sleep(0.5)
+
+print("BrightPi is at max!")
--- src/brightpi-off.py
+++ src/brightpi-off.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 # Reset BrightPi to default and turn off all LEDs
+import time
 
-from brightpi.brightpilib import BrightPi, LED_ALL, OFF
+from brightpi.brightpilib import BrightPi, LED_IR, LED_WHITE, OFF
 
 brightPi = BrightPi()
 
+print("Turning off LED's...")
+brightPi.set_led_on_off(LED_IR, OFF)
+for led in LED_WHITE:
+    brightPi.set_led_on_off(led, OFF)
+    time.sleep(0.5)
+
+print("Resetting BrightPi...")
 brightPi.reset()
-brightPi.set_led_on_off(LED_ALL, OFF)
+
+print("BrightPi has been turned off!")
